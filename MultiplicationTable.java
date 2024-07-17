32f1b95588b85f3d996d6c4ffdad5cf355cd1917
SHUBHAKANT NAYAK <146442931+nayakshubhakant9@users.noreply.github.com>
2024-07-17 18:37:00 +0530

    First-java

---
+++ MultiplicationTable.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter a number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Print the multiplication table
+        System.out.println("Multiplication Table for " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
