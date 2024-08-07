2999f3c3edf0936a80ab85da01e5dcf7ddc2d7f8
SHUBHAKANT NAYAK <146442931+nayakshubhakant9@users.noreply.github.com>
2024-08-07 17:28:32 +0530

    basic patterns

--- CH - 5 JAVA/Ques_6.java
+++ CH - 5 JAVA/Ques_6.java
@@ -7,7 +7,7 @@ public class Ques_6 {
         {
             for (int j=1; j<=i; j++)
             {
-System.out.print(j+"");
+System.out.print(j+" ");
             }
             System.out.println();
         }
--- CH - 5 JAVA/Ques_7.java
+++ CH - 5 JAVA/Ques_7.java
@@ -1,3 +1,17 @@
 public class Ques_7 {
-    
+    public static void main(String[] args) 
+    {
+        int n = 5;
+
+        // outer loop
+        for(int i=1; i<=n; i++)
+        {
+            // Inner loop
+            for (int j=1; j<=n-i+1; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
