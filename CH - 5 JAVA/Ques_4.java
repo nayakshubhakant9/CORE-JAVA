8177087e889232a73cd3f4885b3e53259f72194d
SHUBHAKANT NAYAK <146442931+nayakshubhakant9@users.noreply.github.com>
2024-08-07 12:08:07 +0530

    patterns

--- CH - 5 JAVA/Ques_4.java
+++ CH - 5 JAVA/Ques_4.java
@@ -1,3 +1,16 @@
 public class Ques_4 {
-    
+    public static void main(String[] args) {
+        int n = 4;
+
+        // outer loop
+        for (int i = n; i >= 1; i--) {
+            // inner loop
+
+            for (int j = 1; j <= i; j++) { // nested for loop 
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
 }
