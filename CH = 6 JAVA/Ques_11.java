e6d930baf15bb05fa508c1b52447b2c21a59acd8
SHUBHAKANT NAYAK <146442931+nayakshubhakant9@users.noreply.github.com>
2024-08-09 17:56:17 +0530

    error

--- CH = 6 JAVA/Ques_11.java
+++ CH = 6 JAVA/Ques_11.java
@@ -1,5 +1,22 @@
-
+// solid rhombus
 
 public class Ques_11 {
-    
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - 1; j++) {
+
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
 }
---
+++ CH = 6 JAVA/tempCodeRunnerFile.java
@@ -0,0 +1,5 @@
+     // stars
+            for (int j = 1; j <= 5; j++) {
+                System.out.println("*");
+            }
+            System.out.println();
